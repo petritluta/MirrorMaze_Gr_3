1789f493eb74ce78a572dcf7c6f1a1a3e6b5c81f
Lendrit Ibrahimi <lendritibrahimi@outlook.com>
2022-06-05 00:13:15 +0200

    added a solution tester

--- MirrorMazeBruteForce/src/Main.java
+++ MirrorMazeBruteForce/src/Main.java
@@ -1,6 +1,18 @@
+import InputOutput.Reader;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        char[][] mazeArray = null;
+        try {
+            mazeArray = Reader.read("maze.txt").get(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MirrorMaze maze = new MirrorMaze(mazeArray[0].length, mazeArray.length, mazeArray);
+        System.out.println(maze.toString());
+        System.out.println(maze.checkSolution());
     }
 }
--- MirrorMazeBruteForce/src/MirrorMaze.java
+++ MirrorMazeBruteForce/src/MirrorMaze.java
@@ -1,4 +1,6 @@
-public class MirrorMaze {
+import java.io.Serializable;
+
+public class MirrorMaze implements Serializable {
     private final int width;
     private final int height;
 
@@ -7,10 +9,26 @@ public class MirrorMaze {
     private int startingPositionX;
     private int startingPositionY;
 
+    private Direction direction;
+
     public MirrorMaze(int width, int height, char[][] maze) {
         this.width = width;
         this.height = height;
         this.maze = maze;
+
+        getStartingPosition();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder mazeString = new StringBuilder();
+        for (char[] chars : maze) {
+            for (char aChar : chars) {
+                mazeString.append(aChar);
+            }
+            mazeString.append('\n');
+        }
+        return mazeString.toString();
     }
 
     public char[][] getSolution() {
@@ -18,40 +36,76 @@ public class MirrorMaze {
         return null;
     }
 
-    private boolean checkSolution() {
+    //char[][] maze, Direction direction
+    public boolean checkSolution() {
+        int X = startingPositionX;
+        int Y = startingPositionY;
+        System.out.println(direction + " - " + X + "  " + Y);
 
+        while (maze[X][Y] != '*') {
+
+            switch (direction) {
+                case UP -> Y--;
+                case DOWN -> Y++;
+                case RIGHT -> X++;
+                case LEFT -> X--;
+            }
+            if (maze[X][Y] == '/') {
+                switch (direction) {
+                    case UP -> direction = Direction.RIGHT;
+                    case DOWN -> direction = Direction.LEFT;
+                    case LEFT -> direction = Direction.DOWN;
+                    case RIGHT -> direction = Direction.UP;
+                }
+            } else if (maze[X][Y] == '\\') {
+                switch (direction) {
+                    case UP -> direction = Direction.LEFT;
+                    case DOWN -> direction = Direction.RIGHT;
+                    case LEFT -> direction = Direction.UP;
+                    case RIGHT -> direction = Direction.DOWN;
+                }
+            }
+            System.out.println(direction + " - " + X + "  " + Y);
+            if (X == 0 || X == width - 1 || Y == 0 || Y == height - 1) {
+                return true;
+            }
+        }
         return false;
     }
 
     private void getStartingPosition() {
         startingPositionX = 0;
 
-        for (int startingPositionY = 1; startingPositionY < height - 1; startingPositionY++) {
-            if (maze[startingPositionX][startingPositionY] == '.') {
+        for (startingPositionY = 1; startingPositionY < height - 1; startingPositionY++) {
+            if (maze[startingPositionY][startingPositionX] == '.') {
+                direction = Direction.DOWN;
                 return;
             }
         }
 
         startingPositionX = width - 1;
 
-        for (int startingPositionY = 1; startingPositionY < height - 1; startingPositionY++) {
+        for (startingPositionY = 1; startingPositionY < height - 1; startingPositionY++) {
             if (maze[startingPositionX][startingPositionY] == '.') {
+                direction = Direction.UP;
                 return;
             }
         }
 
         startingPositionY = 0;
 
-        for (int startingPositionX = 1; startingPositionX < width - 1; startingPositionX++) {
+        for (startingPositionX = 1; startingPositionX < width - 1; startingPositionX++) {
             if (maze[startingPositionX][startingPositionY] == '.') {
+                direction = Direction.RIGHT;
                 return;
             }
         }
 
         startingPositionY = height - 1;
 
-        for (int startingPositionX = 1; startingPositionX < width - 1; startingPositionX++) {
+        for (startingPositionX = 1; startingPositionX < width - 1; startingPositionX++) {
             if (maze[startingPositionX][startingPositionY] == '.') {
+                direction = Direction.LEFT;
                 return;
             }
         }
