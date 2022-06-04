e09a40800ebdb52658beea02f6ede8fbc267f40f
Lendrit Ibrahimi <lendritibrahimi@outlook.com>
2022-06-04 23:06:27 +0200

    added a new class to the mirror maze

---
+++ MirrorMazeBruteForce/src/MirrorMaze.java
@@ -0,0 +1,31 @@
+public class MirrorMaze {
+    private final int width;
+    private final int height;
+
+    private final char[][] maze;
+
+    private int startingPositionX;
+    private int startingPositionY;
+
+    public MirrorMaze(int width, int height, char[][] maze) {
+        this.width = width;
+        this.height = height;
+
+        this.maze = maze;
+    }
+
+    public char[][] getSolution() {
+
+        return null;
+    }
+
+    private boolean checkSolution() {
+
+        return false;
+    }
+
+    private void getStartingPosition() {
+    
+    }
+
+}
