a96057197d0e0d617bc3a1750c75a6a6aa107d5b
Kastriot Salihu <49651929+KastriotSalihu@users.noreply.github.com>
2022-06-04 23:25:03 +0200

    read multiple mazes

--- MirrorMazeBruteForce/src/InputOutput/Reader.java
+++ MirrorMazeBruteForce/src/InputOutput/Reader.java
@@ -1,27 +1,46 @@
 package InputOutput;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Reader {
 
-    public static char[][] read(String path) throws IOException {
-        List<char[]> maze = new ArrayList<>();
+    public static List<char[][]> read(String path) throws IOException {
+        List<char[][]> mazes = new ArrayList<>();
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader(path));
-            String line = reader.readLine();
-            while (line != null) {
-                maze.add(line.toCharArray());
-                // read next line
-                line = reader.readLine();
+            while (true) {
+                String line = reader.readLine();
+
+                List<Integer> ints = Arrays.stream(line.split(" "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+
+                if (ints.get(0) == -1 || ints.get(1) == -1)
+                    break;
+                mazes.add(readMaze(reader, ints.get(0), ints.get(1)));
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return (char[][]) maze.toArray();
+        return mazes;
+    }
+
+    private static char[][] readMaze(BufferedReader reader, int width, int height) throws IOException {
+        char[][] maze = new char[width][height];
+        String line = reader.readLine();
+        int lineIndex = 0;
+        while (line != null) {
+            maze[lineIndex++] = line.toCharArray();
+            line = reader.readLine();
+        }
+        return maze;
     }
 }
