f6f3c2c4df3824d91c6345d3604faaa2104b3dca
=Petrit Luta <petrit.luta@gmail.com>
2022-06-04 21:48:41 +0200

    Grap package added

---
+++ src/graph/Graph.java
@@ -0,0 +1,44 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+  private Map<Vertex, List<Vertex>> vertices;
+
+
+  public  void addVertex(String label){
+    Vertex newVertex=new Vertex(label);
+
+    this.vertices.putIfAbsent(newVertex, new ArrayList<>());
+  }
+
+  public void removeVertex(String label)
+  {
+   Vertex vertex=new Vertex(label);
+
+   this.vertices.values().forEach(e->e.remove(vertex));
+   this.vertices.remove(vertex);
+  }
+
+  public void addEdge(String label1,String label2)
+  {
+    Vertex vertex1=new Vertex(label1);
+    Vertex vertex2=new Vertex(label2);
+
+    this.vertices.get(vertex1).add(vertex2);
+    this.vertices.get(vertex2).add(vertex1);
+
+  }
+
+  public void removeEdge(String label1,String label2)
+  {
+    Vertex vertex1=new Vertex(label1);
+    Vertex vertex2=new Vertex(label2);
+
+    
+  }
+
+
+}
---
+++ src/graph/Vertex.java
@@ -0,0 +1,20 @@
+package graph;
+
+
+public class Vertex {
+  private String label;
+
+
+  public Vertex(String label)
+  {
+    this.label=label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+}
